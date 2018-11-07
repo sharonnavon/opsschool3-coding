08eeb2c9218f00762da9da70cf9bb2ef589b4503
sharonnavon <sharon.nik@gmail.com>
2018-11-07 15:06:11 +0200

    Adding reviewer

--- home-assignments/session1/s1_ex1.py
+++ home-assignments/session1/s1_ex1.py
@@ -46,4 +46,3 @@ with open(output_file, 'w') as outfile:
     write_buckets_and_ppl(buckets[2], buckets[3], outfile)
     write_buckets_and_ppl(buckets[3], oldest_age, outfile, operator=operator.le)
 
-####################
